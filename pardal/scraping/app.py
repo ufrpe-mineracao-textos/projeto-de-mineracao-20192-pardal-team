3981a98b10597be9335e467918b646d983038751
Edilson Alves <email@edilsonalves.com>
2019-10-16 13:25:11 -0300

    Change output directory structure

--- pardal/scraping/app.py
+++ pardal/scraping/app.py
@@ -6,7 +6,7 @@ c.Search = '#politica'
 c.Format = 'Tweet id: {id} | Tweet: {tweet}'
 c.Lang = 'pt'
 c.Limit = 5000
-c.Output = 'datasets/positives'
+c.Output = 'datasets/politics'
 c.Store_csv = True
 
 twint.run.Search(c)
