1c5b8fa15db46ff39a73a1e1b604a2d4c2578e6e
Edilson Alves <email@edilsonalves.com>
2019-12-02 13:26:11 -0300

    Add authentication function

--- pardal/application/app.py
+++ pardal/application/app.py
@@ -1,6 +1,22 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
+
+class TwitterClient():
+    def __init__(self):
+        consumer_key = os.getenv('CONSUMER_KEY')
+        consumer_secret = os.getenv('CONSUMER_SECRET')
+        access_token = os.getenv('ACCESS_TOKEN')
+        access_token_secret = os.getenv('ACCESS_TOKEN_SECRET')
+
+        try:
+            self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+            self.auth.set_access_token(access_token, access_token_secret)
+            self.api = tweepy.API(self.auth)
+        except:
+            print('Error: Authentication Failed')
 
 
 @app.route('/')
